8369caeb7e86a9d97a4ca9ad10036af01431f98f
mjdurandar <mjdurandar>
2025-04-02 22:17:34 +0800

    fix ui

--- resources/js/Layouts/PickaWinnerLayout.vue
+++ resources/js/Layouts/PickaWinnerLayout.vue
@@ -3,7 +3,7 @@ import { Head } from '@inertiajs/vue3';
 </script>
 
 <template>
-    <div class="min-h-screen bg-black">
+    <div class="min-h-screen" style="background-color: #151515;">
         <main>
             <slot />
         </main>
