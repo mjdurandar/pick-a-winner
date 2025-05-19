0298e34fb7fec4a2fc42f169ebf4bb4138488f4b
mjdurandar <mjdurandarjr@gmail.com>
2025-05-19 20:41:08 +0800

    fix: test push

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -91,7 +91,7 @@ const submit = () => {
                     :class="{ 'opacity-25': form.processing }"
                     :disabled="form.processing"
                 >
-                    Log in (TEST)
+                    Log in (TESTS)
                 </PrimaryButton>
             </div>
         </form>
