76da1e70378a3afc5c1501c6ab9ee61b2ce451fe
MJ Durandar <mjdurandarjr@gmail.com>
2026-07-13 16:51:08 +0800

    fix: make the guide public

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -54,6 +54,7 @@ class HandleInertiaRequests extends Middleware
             'forgot-password', 
             'reset-password/*', 
             'form/*',
+            'host-guide/*',
             'pickawinner',
             'pickawinner/*',
             'api/events/*/locations',
